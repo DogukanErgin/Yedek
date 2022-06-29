1c6c138698bd5a15ed226530d90d774152593532
DogukanErgin <d35ergin@hotmail.com>
2022-06-29 18:55:54 +0300

    güncelleme

--- proje1sitedenveriçekme/güncelvericekmecözüm/güncelvericekme/sqlsinif.cs
+++ proje1sitedenveriçekme/güncelvericekmecözüm/güncelvericekme/sqlsinif.cs
@@ -11,7 +11,7 @@ namespace güncelvericekme
     {
         public SqlConnection baglantimetod()
         {
-            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-7S1L2OC\\SQLEXPRESS;Initial Catalog=Dbo_Rig;Integrated Security=True");
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-7S1L2OC;Initial Catalog=Tez1;Integrated Security=True");
             baglanti.Open();
             return baglanti;
         }
